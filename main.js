a8d8e31fb2362e0bf4a3002fec11957683f2bb08
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2024-06-25 17:22:12 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,294 @@
+/*================*/
+/* text scroll js */
+/*================*/
+
+/* global constants */
+const inputText = document.getElementById('inputText');
+const canvasWidth = document.getElementById('canvasWidth');
+const canvasHeight = document.getElementById('canvasHeight');
+const fontSize = document.getElementById('fontSize');
+const scrollSpeed = document.getElementById('scrollSpeed');
+const paddingX = document.getElementById('paddingX');
+const paddingY = document.getElementById('paddingY');
+const lineHeight = document.getElementById('lineHeight');
+const fps = document.getElementById('fps');
+const backgroundColor = document.getElementById('backgroundColor');
+const textColor = document.getElementById('textColor');
+
+const playButton = document.getElementById('playButton');
+const previewDownloadButton = document.getElementById('previewDownloadButton');
+
+const canvas = document.getElementById('canvas1'); 
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth / 5;
+canvas.height = window.innerHeight / 2;
+canvas.style.backgroundColor = backgroundColor.value; 
+
+// Initially set recorder state to null 
+let recorderState = null;
+
+/* class ScrollText */
+class ScrollText {
+	// private property
+	#totalTextHeight = 0;
+	#wrappedText;
+
+	// constructor method
+	constructor (inputText) {
+		this.inputText = inputText.split('\n');		
+		this.fontStyle = 'bold san-serif';
+		this.fontSize = fontSize.value;
+		this.scrollSpeed = scrollSpeed.value;
+		this.backgroundColor = backgroundColor.value;
+		this.textColor = textColor.value;
+		this.paddingX = paddingX.value;
+		this.paddingY = paddingY.value;
+		this.lineHeight = lineHeight.value;
+
+		this.interval = 1000/fps.value;
+		this.lastTimeStamp = 0;
+		this.timer = 0;
+
+		this.recorderState = 'initialized';
+		this.recorder = null;
+		this.animationFrameReference = null;
+
+		canvas.width = canvasWidth.value;
+		canvas.height = canvasHeight.value;
+		canvas.style.backgroundColor = this.backgroundColor;
+		this.x = this.paddingX / 2;
+		this.y = 0;	
+		this.wrapText();	
+		this.calcTotalTextHeight();
+		this.currentY = canvas.height;
+	}
+	// scroll method
+	scroll(timeStamp) {				
+		
+		// calculating delta time for uniform play in all other devices
+		const deltaTime = timeStamp - this.lastTimeStamp;
+
+		// setting current time stamp for next animation frame
+		this.lastTimeStamp = timeStamp;	
+
+		// drawing when only timer is greater than the interval otherwise idle until timer gets reach
+		if (this.timer > this.interval) {
+
+			// clear everything in canvas before start next frame
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			
+			// fill background
+			ctx.fillStyle = this.backgroundColor;
+			ctx.fillRect(0, 0, canvas.width, canvas.height);	
+
+			// setting font style
+			ctx.font = `${this.fontSize}px ${this.fontStyle}`;		
+			
+			// setting initial y co-ordinate to be height of canvas			
+			this.y = this.currentY;
+			
+			// iterate through rearranged wrapped text for given details
+			for (const line of this.#wrappedText) {
+
+				// scrolling text fill
+				ctx.fillStyle = this.textColor;
+				ctx.fillText(line, this.x, this.y);
+				
+				// increasing the y co-ordinate to move next line to visible area of canvas
+				this.y += parseInt(this.fontSize) + parseInt(this.lineHeight);
+				
+				// filling padding vertical area
+				ctx.fillStyle = this.backgroundColor;
+				ctx.fillRect(0, 0, canvas.width, this.paddingY/2);
+				ctx.fillRect(0, canvas.height - this.paddingY/2, canvas.width, this.paddingY/2);
+
+			}
+
+			// updating current y co-ordinate value 
+			this.currentY -= this.scrollSpeed;
+
+		} else {
+			// updating idle timer for requested fps 
+			this.timer += Math.floor(deltaTime);
+		}
+
+		// reset y co-ordinate to animate again
+		if (this.currentY + this.totalTextHeight + (this.paddingY) < 0 ) {
+			this.currentY = canvas.height;
+
+			// if this scroll animation request by recorder event then need to stop after completing animation
+			if (this.recorder != null) {				
+				this.recorder.stop();
+				setTimeout(this.stopAnimation.bind(this), 1000);
+			}
+		}
+
+		// saving animation reference to stop or cancelAnimationFrame
+		this.animationFrameReference = window.requestAnimationFrame(this.scroll.bind(this));
+	}
+	// stopAnimation method
+	stopAnimation () {
+		// canceling or stopping animation
+		window.cancelAnimationFrame(this.animationFrameReference);
+	}	
+	// calcTotalTextHeight method
+	calcTotalTextHeight() {
+		// calculating total height that going to occupy by processed text
+		this.totalTextHeight = parseFloat(this.paddingY);
+		for (const line of this.#wrappedText) {
+			this.totalTextHeight += parseFloat(this.fontSize) + parseFloat(this.lineHeight);
+		}
+	}
+	// wrapText method
+	wrapText() {
+		// getting input text split by new lines
+		let lines = this.inputText;
+		
+		// assigning font style to calculate total width and height taken in the canvas
+		ctx.font = `${this.fontSize}px ${this.fontStyle}`;	
+		
+		// created empty lines array to store new lines
+		let tempLines = [];
+
+		// iterate through all lines of text that given
+		for (const line of lines) {
+			// split by space that consider as words
+			const words = line.split(' ');
+
+			// initialize empty current line array for each iteration
+			let currentLine = [];
+
+			// iterate through each word in the line
+			for (const word of words) {
+				// measuring text metrics inside canvas
+				const metrics = ctx.measureText(currentLine.join(' ') + word + ' ');
+				
+				// calculating line width taken inclusive of padding in horizontal
+				const lineWidth = metrics.width + this.paddingX;
+				
+				// if lines width is more than the allotted width then it will be a new line
+				if (lineWidth >= (canvas.width - this.paddingX)) {
+					tempLines.push(currentLine.join(' '));
+					currentLine = [];
+				}
+
+				// pushing last line
+				currentLine.push(word);
+			}
+
+			// pushing all new processed lines into temp lines		
+			tempLines.push(currentLine.join(' '));
+		}
+
+		// setting into class properties for animation
+		this.#wrappedText = tempLines;
+
+	}
+}
+
+
+/* class preference */
+class PreferenceHandler {
+	// constructor method
+	constructor () {		
+		this.isAnimationPlaying = false;
+		this.object = null;
+	}
+
+	// setAnimationObject method
+	setAnimationObject(object) {
+		// setting animation object for further reference
+		this.object = object;		
+	}
+
+	// getAnimationState method
+	getAnimationState() {
+		// which returns true or false set to this class properties
+		return this.isAnimationPlaying;
+	}
+
+	// getAnimationObject method
+	getAnimationObject() {
+		// which returns animation class object
+		return this.object;
+	}
+}
+
+/* play button event listener */
+playButton.addEventListener('click', function(e) {
+	recorderState = null;
+	animateScroll();
+});
+
+// initializing PreferenceHandler class object
+const preferenceObj = new PreferenceHandler();
+
+
+/* preview and download button event listener */
+previewDownloadButton.addEventListener('click', function(e) {
+	// disabling button to avoid click when starts processing
+	previewDownloadButton.disabled = true;
+	// Capture canvas as video stream
+	const stream = canvas.captureStream(fps.value);
+
+	// Use MediaRecorder to record the stream
+	const recorder = new MediaRecorder(stream);
+	const videoChunks = [];
+	// stores stream video chunks into array of chunks
+	recorder.ondataavailable = (e) => videoChunks.push(e.data);
+	// recorder event when its stops
+	recorder.onstop = () => {
+		// creating video blob data
+		const videoBlob = new Blob(videoChunks, { type: 'video/webm' });
+		// creating video object URL to download
+		const videoUrl = URL.createObjectURL(videoBlob);
+
+		// Create a downloadable link
+		const link = document.createElement('a');
+		link.href = videoUrl;
+		link.download = 'canvas_video.webm';
+		link.click();
+
+		// Revoke object URL to avoid memory leaks
+		URL.revokeObjectURL(videoUrl);
+
+		// enabling button for next time use
+		previewDownloadButton.disabled = false;
+
+	};
+
+	// starting record
+	recorder.start();	
+
+	animateScroll(recorder);
+});
+
+
+// animation function
+function animateScroll(recorder=null) {
+
+	// if this function is called by play or preview then need to stop the canvas animation frames
+	if (preferenceObj.isAnimationPlaying && preferenceObj.object != null) {
+		preferenceObj.object.stopAnimation();
+	}
+
+	// getting user text input
+	let text = inputText.value;
+
+	// creating ScrollText class animation object 
+	const aniObj = new ScrollText(text);
+
+	// setting recorder reference
+	aniObj.recorder = recorder;
+
+	// calling scroll method to start animation
+	aniObj.scroll(0);
+
+	// setting ScrollText class object for further reference
+	preferenceObj.setAnimationObject(aniObj);
+
+	// setting animation play as true means playing and false means stopping
+	preferenceObj.isAnimationPlaying = true;
+}
+
+
